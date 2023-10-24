e762a498c34bc5e2cc1a1f107d4f5b3ff7fa89b3
Andrea Manuel <andreamanuel2207@gmail.com>
2023-10-24 11:17:52 -0600

    Install gem rack-cors to allow api request from localhost:3001

--- Gemfile
+++ Gemfile
@@ -64,3 +64,5 @@ group :test do
 end
 
 gem 'rswag'
+
+gem 'rack-cors'
--- config/application.rb
+++ config/application.rb
@@ -16,12 +16,11 @@ module HelloRailsBackEnd
     # Common ones are `templates`, `generators`, or `middleware`, for example.
     config.autoload_lib(ignore: %w(assets tasks))
 
-    # Configuration for the application, engines, and railties goes here.
-    #
-    # These settings can be overridden in specific environments using the files
-    # in config/environments, which are processed later.
-    #
-    # config.time_zone = "Central Time (US & Canada)"
-    # config.eager_load_paths << Rails.root.join("extras")
+    config.middleware.insert_before 0, Rack::Cors do
+      allow do
+        origins 'http://localhost:3001'  # O agrega el dominio de tu aplicación React
+        resource '*', headers: :any, methods: [:get, :post, :put, :patch, :delete, :options]
+      end
+    end
   end
 end
